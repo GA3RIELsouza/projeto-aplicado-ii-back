d765a6734a8a4c7ed66c9ad4cf16e12e06a9bd3a
Gabriel Leandro de Souza <gabrielleandrodesouza11@gmail.com>
2025-06-10 22:43:35 -0300

    ajuste

--- Entities/ProductInInventory.cs
+++ Entities/ProductInInventory.cs
@@ -5,12 +5,10 @@ namespace Projeto_Aplicado_II_API.Entities
     public class ProductInInventory : BranchOwnedEntityBase
     {
         public uint ProductId { get; set; }
-        public uint SupplierId { get; set; }
         public DateOnly ManufacturingDate { get; set; }
         public uint? SaleItemId { get; set; }
 
         public virtual Product? Product { get; set; }
-        public virtual Supplier? Supplier { get; set; }
         public virtual SaleItem? SaleItem { get; set; }
     }
 }
