96db062d3711dccaba83eedd663943d7705b7460
Gabriel Leandro de Souza <gabriel.souza@berkan.com.br>
2025-06-16 17:24:43 -0300

    ajuste

--- dockers.bat
+++ dockers.bat
@@ -1,7 +1,7 @@
 :: Configuração do console
 @echo off
 title Projeto Aplicado II - Docker setup
-color 0e
+color 0f
 
 :: Variáveis
 set NOME_NETWORK=projeto-aplicado-ii
@@ -14,6 +14,8 @@ set NOME_DOCKER_BANCO=projeto-aplicado-ii-sqlserver
 set PORTA_DOCKER_BANCO=1433
 
 :: Excluir anteriores
+echo $ Limpando containers anteriores...
+
 docker stop %NOME_DOCKER_BANCO%
 docker rm %NOME_DOCKER_BANCO%
 
@@ -24,12 +26,14 @@ docker rm %NOME_DOCKER_API%
 docker network rm %NOME_NETWORK%
 docker network create %NOME_NETWORK%
 
-cls
-
 :: SQL Server
+echo.
+echo $ Criando o container do SQL Server...
+
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Gabriel@13242872924" -p %PORTA_DOCKER_BANCO%:%PORTA_DOCKER_BANCO% --network %NOME_NETWORK% --name %NOME_DOCKER_BANCO% -d mcr.microsoft.com/mssql/server:2019-latest
 
-echo Aguardando inicializacao do SQL Server...
+echo.
+echo $ Aguardando a inicializacao do SQL Server...
 :aguardando_banco
 docker logs %NOME_DOCKER_BANCO% 2>&1 | findstr /C:"Recovery is complete." >nul
 
@@ -37,10 +41,16 @@ if errorlevel 1 (
     timeout /t 1 >nul
     goto aguardando_banco
 )
-echo SQL Server inicializado com sucesso.
 
 :: API
+echo.
+echo $ Criando o container da API...
+
 docker build -t %NOME_DOCKERFILE%:latest .
 docker run -d -p %PORTA_DOCKER_API%:%PORTA_DOCKER_API% --network %NOME_NETWORK% --name %NOME_DOCKER_API% %NOME_DOCKERFILE%
+
+:: Fim
+echo.
+echo $ Containers inicializados com sucesso!
 
 pause
